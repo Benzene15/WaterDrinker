5bbededcc3c0bc9c87e502c12021e7b841234ea5
Ben Cameron <camer343@umn.edu>
2020-12-30 16:25:43 -0600

    Adding the top left canoodle

--- app/src/main/java/com/example/waterdrinker/MainActivity.java
+++ app/src/main/java/com/example/waterdrinker/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.waterdrinker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    int startingValue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText waterDrank = (EditText)findViewById(R.id.waterDrank);
+        waterDrank.setText("0");
     }
 }
