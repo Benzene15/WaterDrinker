613917c9f645a451cd762b1cf70735d5328f067d
Ben Cameron <51214565+Benzene15@users.noreply.github.com>
2021-01-12 01:41:01 -0600

    Adding reset and undo buttons

--- app/src/main/java/com/example/waterdrinker/MainActivity.java
+++ app/src/main/java/com/example/waterdrinker/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.ViewAnimator;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-
+import  java.lang.Math;
 import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
@@ -20,6 +20,7 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
     int startingValue = 0;
     int lastValue=0;
+    int WEIGHT=200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         date.setText(currentDateandTime.toString());
         EditText quote = (EditText)findViewById(R.id.Quote);
         quote.setText("Stay hydrated, stay happy");
+        updatePercentage();
     }
     public void addWater(View view){
         TextInputEditText waterInput = (TextInputEditText) findViewById(R.id.waterAdded);
@@ -47,6 +49,32 @@ public class MainActivity extends AppCompatActivity {
             waterSoFar += waterAdded;
             lastValue = waterAdded;
             waterDrank.setText(Integer.toString(waterSoFar));
+            updatePercentage();
+        }
+    }
+    public void undo(View view){
+        EditText waterDrank = (EditText)findViewById(R.id.waterDrank);
+        int waterSoFar = Integer.parseInt(waterDrank.getText().toString());
+        waterSoFar-=lastValue;
+        lastValue=0;
+        waterDrank.setText(Integer.toString(waterSoFar));
+        updatePercentage();
+    }
+    public void reset(View view){
+        EditText waterDrank = (EditText)findViewById(R.id.waterDrank);
+        waterDrank.setText("0");
+        updatePercentage();
+    }
+    public void updatePercentage(){
+        EditText waterPercentage = (EditText)findViewById((R.id.percentComplete));
+        EditText waterDrank  =(EditText)findViewById(R.id.waterDrank);
+        int waterSoFar = Integer.parseInt(waterDrank.getText().toString());
+        int percent = (int)Math.min(100,(waterSoFar/(WEIGHT/2.0))*100);
+        if(percent==100){
+            waterPercentage.setText("Yay!");
+        }
+        else{
+            waterPercentage.setText(Integer.toString(percent));
         }
     }
 }
